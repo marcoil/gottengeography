marcoil/gottengeography

commit d3056cb6d44282529f4dbb272313fbfc2ad80263

Stop creating duplicate Photograph objects during photo reload.
This commit causes existing Photograph objects to be reused in the event that the user reloads an already open file.

gg/datatypes.py
@@ -62,7 +62,6 @@ class Photograph(ReadableDictionary):
     
     def __init__(self, filename, cache, callback, thumb_size=200):
         """Initialize new Photograph object's attributes with default values."""
-        gps = 'Exif.GPSInfo.GPS'
         self.filename = filename
         self.cache    = cache
         self.callback = callback
@@ -70,11 +69,17 @@ class Photograph(ReadableDictionary):
         for key in [ 'timestamp', 'altitude', 'latitude', 'longitude',
         'marker', 'iter', 'timezone' ] + iptc_keys:
             self[key] = None
+        self.thm_size = thumb_size
+        self.read()
+    
+    def read(self):
+        """Load exif data from disk."""
+        gps = 'Exif.GPSInfo.GPS'
         try:
-            self.exif = pyexiv2.ImageMetadata(filename)
+            self.exif = pyexiv2.ImageMetadata(self.filename)
             self.exif.read()
             self.thumb = GdkPixbuf.Pixbuf.new_from_file_at_size(
-                filename, thumb_size, thumb_size)
+                self.filename, self.thm_size, self.thm_size)
         except:
             raise IOError
         self.calculate_timestamp()
@@ -88,18 +93,19 @@ class Photograph(ReadableDictionary):
                 [self.exif[gps + 'LongitudeRef'].value]
             )
         except KeyError:
-            pass
+            self.latitude  = None
+            self.longitude = None
         try:
             self.altitude = self.exif[gps + 'Altitude'].value.to_float()
             if int(self.exif[gps + 'AltitudeRef'].value) > 0:
                 self.altitude *= -1
         except:
-            pass
+            self.altitude = None
         for iptc in iptc_keys:
             try:
                 self[iptc] = self.exif['Iptc.Application2.' + iptc].values[0]
             except KeyError:
-                pass
+                self[iptc] = None
     
     def calculate_timestamp(self):
         """Determine the timestamp based on the currently selected timezone.

gg/app.py
@@ -271,18 +271,17 @@ class GottenGeography:
         
         Raises IOError if filename refers to a file that is not a photograph.
         """
-        photo = Photograph(filename, self.cache, self.modify_summary)
-        photo.update( {
-            'iter':   self.liststore.append([None] * 4),
-            'marker': self.add_marker(filename)
-        } if filename not in self.photo else {
-            'iter':   self.photo[filename].iter,
-            'marker': self.photo[filename].marker
-        } )
-        
+        if filename not in self.photo:
+            self.photo[filename] = Photograph(filename, self.cache, self.modify_summary)
+            self.photo[filename].update( {
+                'iter':   self.liststore.append([None] * 4),
+                'marker': self.add_marker(filename)
+            } )
+        else:
+            self.photo[filename].read()
+        photo = self.photo[filename]
         photo.position_marker()
-        self.modified.discard(self.photo.get(filename, None))
-        self.photo[filename] = photo
+        self.modified.discard(photo)
         self.liststore.set_value(photo.iter, self.PATH,      photo.filename)
         self.liststore.set_value(photo.iter, self.THUMB,     photo.thumb)
         self.liststore.set_value(photo.iter, self.TIMESTAMP, photo.timestamp)

commit ad7d9342294453f57a98a4e2a1255a80654f9da7

Fix reloading of Photograph objects.
I broke them back in d3056cb6d44282529f4dbb272313fbfc2ad80263.

gg/datatypes.py
@@ -93,20 +93,22 @@ class Photograph(ReadableDictionary):
     
     def __init__(self, filename, cache, callback, thumb_size=200):
         """Initialize new Photograph object's attributes with default values."""
+        self.exif     = ImageMetadata(filename)
         self.filename = filename
         self.geonamer = cache
         self.callback = callback
-        self.manual   = False
-        for key in [ 'timestamp', 'altitude', 'latitude', 'longitude',
-        'marker', 'iter', 'timezone' ] + iptc_keys:
-            self[key] = None
         self.thm_size = thumb_size
+        self.marker   = None
+        self.iter     = None
         self.read()
     
     def read(self):
         """Load exif data from disk."""
+        for key in [ 'timestamp', 'altitude', 'latitude', 'longitude',
+        'timezone' ] + iptc_keys:
+            self[key] = None
+        self.manual = False
         try:
-            self.exif = ImageMetadata(self.filename)
             self.exif.read()
             self.thumb = GdkPixbuf.Pixbuf.new_from_file_at_size(
                 self.filename, self.thm_size, self.thm_size)
@@ -123,19 +125,18 @@ class Photograph(ReadableDictionary):
                 [self.exif[gps + 'LongitudeRef'].value]
             )
         except KeyError:
-            self.latitude  = None
-            self.longitude = None
+            pass
         try:
             self.altitude = self.exif[gps + 'Altitude'].value.to_float()
             if int(self.exif[gps + 'AltitudeRef'].value) > 0:
                 self.altitude *= -1
         except:
-            self.altitude = None
+            pass
         for iptc in iptc_keys:
             try:
                 self[iptc] = self.exif['Iptc.Application2.' + iptc].values[0]
             except KeyError:
-                self[iptc] = None
+                pass
     
     def calculate_timestamp(self):
         """Determine the timestamp based on the currently selected timezone.

gg/testsuite.py
@@ -90,6 +90,16 @@ class GottenGeographyTester(unittest.TestCase):
             self.assertIsNone(self.gui.photo[filename].altitude)
             self.assertIsNone(self.gui.photo[filename].latitude)
             self.assertIsNone(self.gui.photo[filename].longitude)
+            self.assertFalse(self.gui.photo[filename].manual)
+            self.gui.photo[filename].manual = True
+            self.gui.photo[filename].latitude = 10.0
+            self.gui.photo[filename].altitude = 650
+            self.gui.photo[filename].latitude = 45.0
+            self.gui.photo[filename].read()
+            self.assertIsNone(self.gui.photo[filename].altitude)
+            self.assertIsNone(self.gui.photo[filename].latitude)
+            self.assertIsNone(self.gui.photo[filename].longitude)
+            self.assertFalse(self.gui.photo[filename].manual)
             self.assertEqual(filename, self.gui.photo[filename].marker.get_name())
             self.assertEqual(self.gui.photo[filename].timestamp,
                 self.gui.liststore.get_value(