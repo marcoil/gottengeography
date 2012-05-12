b3588ca26041ad8da703c318eba757d111f2996d
Robert Bruce Park <rbpark@exolucere.ca>
2012-05-12 15:01:53 -0500

    Total overhaul of setup.py so that RPMs correctly include translations.

--- setup.py
+++ setup.py
@@ -2,46 +2,42 @@
 
 from os import makedirs, system
 from os.path import join, basename, dirname, exists
-from distutils.command.install_data import install_data
 from distutils.dep_util import newer
 from distutils.core import setup
-from distutils.log import info
 from glob import glob
 
 from gg.version import *
 
-class InstallData(install_data):
-    """This class is copied from setup.py in Rapid Photo Downloader 0.3.4
-    by Damon Lynch <damonlynch@gmail.com>.
-    """
-    def run (self):
-        self.data_files.extend(self._compile_po_files())
-        install_data.run(self)
+data_files = [
+    ('share/applications', ['data/gottengeography.desktop']),
+    ('share/doc/%s' % PACKAGE, ['README.md', 'AUTHORS', 'COPYING'])
+]
+
+"""Compile all human-readable .po files into system-usable .mo files.
+This uses msgfmt and is why you need intltool installed for a source install."""
+for po in glob(join('po', '*.po')):
+    lang = basename(po[:-3])
+    mo = join('locale', lang, 'LC_MESSAGES', '%s.mo' % PACKAGE)
     
-    def _compile_po_files (self):
-        data_files = []
-        
-        PO_DIR = 'po'
-        for po in glob(join(PO_DIR, '*.po')):
-            lang = basename(po[:-3])
-            mo = join('build', 'mo', lang, '%s.mo' % PACKAGE)
-            
-            directory = dirname(mo)
-            if not exists(directory):
-                info('creating %s' % directory)
-                makedirs(directory)
-            
-            if newer(po, mo):
-                # True if mo doesn't exist
-                cmd = 'msgfmt -o %s %s' % (mo, po)
-                info('compiling %s -> %s' % (po, mo))
-                if system(cmd) != 0:
-                    raise SystemExit('Error while running msgfmt')
-                
-                dest = dirname(join('share', 'locale', lang, 'LC_MESSAGES', '%s.mo' % PACKAGE))
-                data_files.append((dest, [mo]))
-        
-        return data_files
+    directory = dirname(mo)
+    if not exists(directory):
+        print 'creating %s' % directory
+        makedirs(directory)
+    
+    if newer(po, mo):
+        # True if mo doesn't exist
+        print 'compiling %s -> %s' % (po, mo)
+        if system('msgfmt -o %s %s' % (mo, po)) != 0:
+            raise SystemExit('Error while running msgfmt')
+
+"""This second loop is slightly redundant, but it's necessary because setup.py
+calls itself recursively a couple of different times from inside a FAKEROOT.
+The .po files are only accessible to compile the very first time, but the .mo
+files need to be included in `data_files` every time, otherwise they don't
+make it into the final RPM."""
+for locale in glob(join('locale', '*')):
+    data_files.append(('share/%s/LC_MESSAGES' % locale,
+        [join(locale, 'LC_MESSAGES', '%s.mo' % PACKAGE)]))
 
 setup(
     name=APPNAME,
@@ -63,7 +59,5 @@ and then record those locations into the photos.
     packages=['gg'],
     package_data={'gg': ['ui.glade', 'cities.txt']},
     scripts=['gottengeography'],
-    data_files=[('share/applications', ['data/gottengeography.desktop']),
-                ('share/doc/%s' % PACKAGE, ['README.md', 'AUTHORS', 'COPYING'])],
-    cmdclass={'install_data': InstallData}
+    data_files=data_files
 )
