72719a6ab19d0bdd4bc0bfdd3cb56a8bf70bdb52
Robert Bruce Park <rbpark@exolucere.ca>
2012-05-21 08:54:17 -0500

    pylint of label.py

--- gg/label.py
+++ gg/label.py
@@ -1,4 +1,3 @@
-# GottenGeography - Control the behavior of ChamplainLabels.
 # Copyright (C) 2010 Robert Park <rbpark@exolucere.ca>
 #
 # This program is free software: you can redistribute it and/or modify
@@ -14,12 +13,63 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+"""Control the behavior of ChamplainLabels."""
+
 from __future__ import division
 
 from gi.repository import Gtk, Champlain, Clutter
 from os.path import basename
 
 from common import get_obj, map_view, selected, modified, photos
+
+def update_highlights(selection):
+    """Ensure only the selected labels are highlighted."""
+    selection_exists = selection.count_selected_rows() > 0
+    selected.clear()
+    for photo in photos.values():
+        # Maintain the 'selected' set() for easier iterating later.
+        if selection.iter_is_selected(photo.iter):
+            selected.add(photo)
+        photo.set_label_highlight(photo in selected, selection_exists)
+
+def selection_sensitivity(selection, aply, close, save, revert, left):
+    """Control the sensitivity of various widgets."""
+    sensitive = selection.count_selected_rows() > 0
+    close.set_sensitive(sensitive)
+    aply.set_sensitive(sensitive)
+    save.set_sensitive(  len(modified) > 0)
+    revert.set_sensitive(len(modified & selected) > 0)
+    if len(photos) > 0: left.show()
+    else:               left.hide()
+
+def clicked(label, event, selection, select_all):
+    """When a ChamplainLabel is clicked, select it in the GtkListStore.
+    
+    The interface defined by this method is consistent with the behavior of
+    the GtkListStore itself in the sense that a normal click will select
+    just one item, but Ctrl+clicking allows you to select multiple.
+    """
+    photo = photos[label.get_name()]
+    assert photo.filename == label.get_name()
+    if event.get_state() & Clutter.ModifierType.CONTROL_MASK:
+        if label.get_selected(): selection.unselect_iter(photo.iter)
+        else:                    selection.select_iter(photo.iter)
+    else:
+        select_all.set_active(False)
+        selection.unselect_all()
+        selection.select_iter(photo.iter)
+
+def drag_finish(label, event):
+    """Update photos with new locations after photos have been dragged."""
+    photo = photos[label.get_name()]
+    photo.set_location(label.get_latitude(), label.get_longitude())
+    photo.manual = True
+    map_view.emit('animation-completed')
+
+def hover(label, event, factor):
+    """Scale a ChamplainLabel by the given factor."""
+    label.set_scale(*[scale * factor for scale in label.get_scale()])
+
 
 class LabelController():
     """Control the behavior and creation of ChamplainLabels."""
@@ -31,8 +81,8 @@ class LabelController():
         self.layer = Champlain.MarkerLayer()
         map_view.add_layer(self.layer)
         
-        self.selection.connect('changed', self.update_highlights)
-        self.selection.connect('changed', self.selection_sensitivity,
+        self.selection.connect('changed', update_highlights)
+        self.selection.connect('changed', selection_sensitivity,
             *[get_obj(name) for name in ('apply_button', 'close_button',
                 'save_button', 'revert_button', 'photos_with_buttons')])
     
@@ -44,59 +94,11 @@ class LabelController():
         label.set_selectable(True)
         label.set_draggable(True)
         label.set_property('reactive', True)
-        label.connect('enter-event', self.hover, 1.05)
-        label.connect('leave-event', self.hover, 1/1.05)
-        label.connect('drag-finish', self.drag_finish)
-        label.connect('button-press', self.clicked, self.selection,
+        label.connect('enter-event', hover, 1.05)
+        label.connect('leave-event', hover, 1/1.05)
+        label.connect('drag-finish', drag_finish)
+        label.connect('button-press', clicked, self.selection,
             self.select_all)
         self.layer.add_marker(label)
         return label
-    
-    def update_highlights(self, selection):
-        """Ensure only the selected labels are highlighted."""
-        selection_exists = selection.count_selected_rows() > 0
-        selected.clear()
-        for photo in photos.values():
-            # Maintain the 'selected' set() for easier iterating later.
-            if selection.iter_is_selected(photo.iter):
-                selected.add(photo)
-            photo.set_label_highlight(photo in selected, selection_exists)
-    
-    def selection_sensitivity(self, selection, aply, close, save, revert, left):
-        """Control the sensitivity of various widgets."""
-        sensitive = selection.count_selected_rows() > 0
-        close.set_sensitive(sensitive)
-        aply.set_sensitive(sensitive)
-        save.set_sensitive(  len(modified) > 0)
-        revert.set_sensitive(len(modified & selected) > 0)
-        if len(photos) > 0: left.show()
-        else:               left.hide()
-    
-    def clicked(self, label, event, selection, select_all):
-        """When a ChamplainLabel is clicked, select it in the GtkListStore.
-        
-        The interface defined by this method is consistent with the behavior of
-        the GtkListStore itself in the sense that a normal click will select
-        just one item, but Ctrl+clicking allows you to select multiple.
-        """
-        photo = photos[label.get_name()]
-        assert photo.filename == label.get_name()
-        if event.get_state() & Clutter.ModifierType.CONTROL_MASK:
-            if label.get_selected(): selection.unselect_iter(photo.iter)
-            else:                    selection.select_iter(photo.iter)
-        else:
-            select_all.set_active(False)
-            selection.unselect_all()
-            selection.select_iter(photo.iter)
-    
-    def drag_finish(self, label, event):
-        """Update photos with new locations after photos have been dragged."""
-        photo = photos[label.get_name()]
-        photo.set_location(label.get_latitude(), label.get_longitude())
-        photo.manual = True
-        map_view.emit('animation-completed')
-    
-    def hover(self, label, event, factor):
-        """Scale a ChamplainLabel by the given factor."""
-        label.set_scale(*[scale * factor for scale in label.get_scale()])
 
