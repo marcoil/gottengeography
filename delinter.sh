e6b6444af1c94e5b94cac270e1fac9047cc914a2
Robert Bruce Park <rbpark@exolucere.ca>
2012-06-14 23:29:47 -0500

    Add some warnings to delinter.sh and make it more flexible.

--- delinter.sh
+++ delinter.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+if [ $1 ]; then
+    CHECK=$1
+else
+    CHECK=gg
+fi
+
 # pylint doesn't actually play very well with Gtk, so
 # let's disable some false positives:
-IGNORE=E0611,E1101
-
-pylint -d $IGNORE --include-ids=y gg
-
-pylint -d $IGNORE --include-ids=y test
+pylint -d E0611,E1101,W0613,W0403,W0142,W0141,W0102,R0903 --include-ids=y $CHECK
